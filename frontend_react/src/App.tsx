cd742f56ee69c8e8e4c6b00657b7d328ffac385e
Tina Wang <wangzongting521@gmail.com>
2023-08-21 21:55:19 +0100

    Create containters for each section of the portfolio

--- frontend_react/src/App.tsx
+++ frontend_react/src/App.tsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import {About, Footer, Header, Projects, Skills, Testimonials, Experiences } from './containter'
 
 function App() {
   return (
-    <div>
-      
+    <div className="app">
+      <Header />
+      <About />
+      <Projects />
+      <Experiences />
+      <Skills />
+      <Testimonials />
+      <Footer />
     </div>
   )
 }
---
+++ frontend_react/src/containter/Experiences/Experiences.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Experiences.scss'
+
+const Experiences = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Experiences
