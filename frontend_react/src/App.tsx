85bfb8d82250c6bd38085cfdb3e44d13a51df006
Tina Wang <wangzongting521@gmail.com>
2023-08-23 20:08:11 +0100

    Add basic navbar function with css

--- frontend_react/src/App.tsx
+++ frontend_react/src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import './App.scss'
 import {About, Footer, Header, Projects, Skills, Testimonials, Experiences } from './containter'
 import {Navbar} from './components'
 
